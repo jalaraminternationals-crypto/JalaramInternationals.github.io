052ad77019e5692f77288cd9bb6441a931372228
jalaraminternationals-crypto <jalaraminternationals@gmail.com>
2025-08-18 15:19:56 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,4 +43,5 @@ if (submenu) submenu.classList.toggle('show');
       }
     }
   </script>
-  <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+  
+
